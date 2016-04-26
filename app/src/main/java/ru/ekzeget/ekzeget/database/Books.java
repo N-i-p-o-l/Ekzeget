dece437ed8e3d9602c453ae15b7ab1ca4be83baa
NArtur <a.norov@metrafor.ru>
2016-04-26 16:04:36 +0500

    All Books

--- app/src/main/java/ru/ekzeget/ekzeget/database/Books.java
+++ app/src/main/java/ru/ekzeget/ekzeget/database/Books.java
@@ -9,23 +9,94 @@ import ru.ekzeget.ekzeget.model.Book;
  */
 public class Books {
 
-  public static List<Book> oldTestamentsList = new ArrayList<>(27);
-  public static List<Book> newTestamentList = new ArrayList<>(50);
-
-  public static List<Book> getOldTestamentList() {
-    oldTestamentsList.add(new Book("От Матфея", "mf", 28));
-    oldTestamentsList.add(new Book("От Марка", "mk", 16));
-    oldTestamentsList.add(new Book("От Луки", "lk", 24));
-
-    return oldTestamentsList;
-  }
+  public static List<Book> oldTestamentsList = new ArrayList<>(50);
+  public static List<Book> newTestamentsList = new ArrayList<>(27);
 
   public static List<Book> getNewTestamentList() {
-    newTestamentList.add(new Book("Бытие", true, "byt", 50));
-    newTestamentList.add(new Book("Исход", true, "is", 40));
-    newTestamentList.add(new Book("Левит", true, "lev", 27));
+    newTestamentsList.add(new Book("От Матфея",   true,  "mf", 28));
+    newTestamentsList.add(new Book("От Марка",    true,  "mk", 16));
+    newTestamentsList.add(new Book("От Луки",     true,  "lk", 24));
+    newTestamentsList.add(new Book("От Иоанна",   true,  "in", 21));
+    newTestamentsList.add(new Book("Деяния",      true,  "deyan", 28));
+    newTestamentsList.add(new Book("Иакова",      true,  "iak", 5));
+    newTestamentsList.add(new Book("1 Петра",     true,  "1pet", 5));
+    newTestamentsList.add(new Book("2 Петра",     true,  "2pet", 3));
+    newTestamentsList.add(new Book("1 Иоанна",    true,  "1in", 5));
+    newTestamentsList.add(new Book("2 Иоанна",    true,   "2in", 1));
+    newTestamentsList.add(new Book("3 Иоанна",    true,   "3in", 1));
+    newTestamentsList.add(new Book("Иуды",        true,   "iud", 1));
+    newTestamentsList.add(new Book("Римлянам",    true,   "rim", 16));
+    newTestamentsList.add(new Book("1 Коринфянам",true,   "1kor", 16));
+    newTestamentsList.add(new Book("2 Коринфянам",true,   "2kor", 13));
+    newTestamentsList.add(new Book("Галатам",     true,   "gal", 6));
+    newTestamentsList.add(new Book("Ефсеням",     true,    "ef", 6));
+    newTestamentsList.add(new Book("Филиппийцам", true,    "flp", 4));
+    newTestamentsList.add(new Book("Колоссянам",  true,    "kol", 4));
+    newTestamentsList.add(new Book("1 Фессалоник",true,    "1fes", 5));
+    newTestamentsList.add(new Book("2 Фессалоник",true,    "2fes", 3));
+    newTestamentsList.add(new Book("1 Тимофею",   true,    "1tim", 6));
+    newTestamentsList.add(new Book("2 Тимофею",   true,    "2tim", 4));
+    newTestamentsList.add(new Book("Титу",        true,    "tit", 3));
+    newTestamentsList.add(new Book("Филимону",    true,    "flm", 1));
+    newTestamentsList.add(new Book("Евреям",      true,    "evr", 13));
+    newTestamentsList.add(new Book("Апокалипсис", true,    "otkr", 22));
 
-    return newTestamentList;
+    return newTestamentsList;
+  }
+
+  public static List<Book> getOldTestamentList() {
+    oldTestamentsList.add(new Book("Бытие", "byt", 50));
+    oldTestamentsList.add(new Book("Исход", "ish", 40));
+    oldTestamentsList.add(new Book("Левит", "lev", 27));
+    oldTestamentsList.add(new Book("Числа", "chis", 36));
+    oldTestamentsList.add(new Book("Второзаконие", "vtor", 34));
+    oldTestamentsList.add(new Book("Иисуса Навина", "nav", 24));
+    oldTestamentsList.add(new Book("Судей", "sud", 21));
+    oldTestamentsList.add(new Book("Руфь", "ruf", 4));
+    oldTestamentsList.add(new Book("1 Царств", "1car", 31));
+    oldTestamentsList.add(new Book("2 Царств", "2car", 24));
+    oldTestamentsList.add(new Book("3 Царств", "3car", 22));
+    oldTestamentsList.add(new Book("4 Царств", "4car", 25));
+    oldTestamentsList.add(new Book("1 Паралипом", "1par", 29));
+    oldTestamentsList.add(new Book("2 Паралипом", "2par", 36));
+    oldTestamentsList.add(new Book("1 Ездры", "1ezd", 10));
+    oldTestamentsList.add(new Book("Неемия", "neem", 13));
+    oldTestamentsList.add(new Book("2 Ездры", "2ezd", 9));
+    oldTestamentsList.add(new Book("Товит", "tov", 14));
+    oldTestamentsList.add(new Book("Иудифь", "iudif", 16));
+    oldTestamentsList.add(new Book("Есфирь", "esf", 10));
+    oldTestamentsList.add(new Book("Иов", "iov", 42));
+    oldTestamentsList.add(new Book("Псалтирь", "ps", 151));
+    oldTestamentsList.add(new Book("Притчи", "pritch", 31));
+    oldTestamentsList.add(new Book("Екклесиаст", "ekkl", 12));
+    oldTestamentsList.add(new Book("Песнь Песней", "pesn", 8));
+    oldTestamentsList.add(new Book("Прем. Соломона", "prem", 19));
+    oldTestamentsList.add(new Book("Сирах", "sir", 51));
+    oldTestamentsList.add(new Book("Исаия", "is", 66));
+    oldTestamentsList.add(new Book("Иеремия", "ier", 52));
+    oldTestamentsList.add(new Book("Плач Иеремии", "plach", 5));
+    oldTestamentsList.add(new Book("Посл. Иеремии", "posl", 1));
+    oldTestamentsList.add(new Book("Варух", "var", 5));
+    oldTestamentsList.add(new Book("Иезекииль", "iez", 48));
+    oldTestamentsList.add(new Book("Даниил", "dan", 14));
+    oldTestamentsList.add(new Book("Осия", "os", 14));
+    oldTestamentsList.add(new Book("Иоиль", "ioil", 3));
+    oldTestamentsList.add(new Book("Амос", "am", 9));
+    oldTestamentsList.add(new Book("Авдий", "avd", 1));
+    oldTestamentsList.add(new Book("Иона", "ion", 4));
+    oldTestamentsList.add(new Book("Михей", "mih", 7));
+    oldTestamentsList.add(new Book("Наум", "naum", 3));
+    oldTestamentsList.add(new Book("Аввакум", "avv", 3));
+    oldTestamentsList.add(new Book("Софония", "sof", 3));
+    oldTestamentsList.add(new Book("Аггей", "agg", 2));
+    oldTestamentsList.add(new Book("Захария", "zah", 14));
+    oldTestamentsList.add(new Book("Малахия", "mal", 4));
+    oldTestamentsList.add(new Book("1 Маккавейская", "1mak", 16));
+    oldTestamentsList.add(new Book("2 Маккавейская", "2mak", 15));
+    oldTestamentsList.add(new Book("3 Маккавейская", "3mak", 7));
+    oldTestamentsList.add(new Book("3 Ездры", "3ezd", 16));
+
+    return oldTestamentsList;
   }
 
 }
